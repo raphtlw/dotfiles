97e9889220426de57d75e84f76274cdca8df3ea1
Raphael Tang <raphpb1912@gmail.com>
2022-03-07 17:14:43 +0800

    A  .local/share/fonts/.uuid A  bin/audio-input.sh A  bin/kill-adobe.sh

---
+++ bin/audio-input.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Audio Input
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🎤
+# @raycast.packageName Toggle Mic
+
+# Documentation:
+# @raycast.description Setup programs for audio input
+# @raycast.author Raphael Tang
+# @raycast.authorURL https://raph.codes
+
+close() {
+  killall obs
+  osascript -e 'quit app "micSwitch"'
+  osascript -e 'quit app "Mechvibes"'
+  pkill -i mechvibes
+  pkill -i mechvibes
+
+  exit 0
+}
+
+# Check if any of those apps are running
+pgrep 'obs|micSwitch|Mechvibes' > /dev/null 2>&1 && close
+
+# Start OBS
+pgrep obs > /dev/null 2>&1 || {
+  open -a OBS --args --collection Audio --profile General --minimize-to-tray
+}
+# Start micSwitch
+pgrep micSwitch > /dev/null 2>&1 || {
+  open -a micSwitch
+}
+# Start Mechvibes
+pgrep Mechvibes > /dev/null 2>&1 || {
+  open -a Mechvibes
+}
+
---
+++ bin/kill-adobe.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# shellcheck disable=SC2009
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Kill Adobe
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🧹
+# @raycast.packageName Utilities
+
+# Documentation:
+# @raycast.description Kill all Adobe-related processes
+# @raycast.author Raphael Tang
+# @raycast.authorURL https://raph.codes
+
+pkill -fi adobe -9
+
+echo "All Adobe-related processes killed ✨"
